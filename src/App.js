387732ad53b8e8a1871b6f944ce99527e2be3912
ankitt01 <ankitrawat0102@gmail.com>
2022-04-05 21:29:47 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="text-3xl font-bold underline">
+        CRED
+      </h1>
+    </div>
+  );
+}
+
+export default App;
