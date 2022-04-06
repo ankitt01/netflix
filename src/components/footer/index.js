355dac451a23cbae79a053dd6d5cba5a0b723f40
ankitt01 <ankitrawat0102@gmail.com>
2022-04-06 12:42:08 +0530

    refactor jumbotron and footer done

---
+++ src/components/footer/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='text-gray-500 text-sm p-14 md:mx-auto lg:w-4/5'>
+        <p className='mb-4'>Questions? Call 000-800-040-1843</p>
+        <div className='grid grid-cols-2 justify-around sm:grid-cols-4 gap-4'>
+            <div>   
+                <p>FAQ</p>
+                <p>Investor Relations</p>
+                <p>Privacy</p>
+                <p>Speed Test</p>
+            </div>
+            <div>   
+                <p>Help Center</p>
+                <p>Jobs</p>
+                <p>Cookie Preference</p>
+                <p>Legal Notice</p>
+            </div>
+            <div className=''>   
+                <p>Account</p>
+                <p>Way to Watch</p>
+                <p>Corporate Information</p>
+                <p>Only on Netflix</p>
+            </div>
+            <div className=''>   
+                <p>Media Center</p>
+                <p>Terms of Use</p>
+                <p>Contact Us</p>
+                
+            </div>
+        </div>
+        <p>Netflix India</p>
+    </div>
+  )
+}
+
+export default Footer
